50e0c7b73ef4e28a1c815b0be30f2d5ab230681a
skipz155 <55126790+skipz155@users.noreply.github.com>
2020-12-04 11:21:07 +0100

    Pagination

--- client/src/app/memes/meme-list/meme-list.component.ts
+++ client/src/app/memes/meme-list/meme-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Console } from 'console';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_models/pagination';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -9,17 +11,28 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemeListComponent implements OnInit {
   members: Member[];
+  pagination: Pagination;
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    //this.members$ = this.memberService.getMembers();
     this.loadMembers();
   }
 
   loadMembers(){
-    this.memberService.getMembers().subscribe(members => {
-      this.members = members;
+    // this.memberService.getMembers().subscribe(members => {
+    //   this.members = members;
+    // });
+
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe(response => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+      console.log('huj');
     });
   }
+
 
 }
