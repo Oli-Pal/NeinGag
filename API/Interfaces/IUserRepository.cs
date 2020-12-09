8bd3097d11036009052bc643cbbd35d8b8d6a247
skipz155 <55126790+skipz155@users.noreply.github.com>
2020-12-09 15:55:28 +0100

    sadasdasd

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -29,13 +29,7 @@ namespace API.Interfaces
 
         void Add<T>(T entity) where T : class;
         void Delete<T>(T entity) where T : class;
-<<<<<<< HEAD
-=======
-
         Task<IEnumerable<int>> GetPhotoLikes(int id);
-<<<<<<< HEAD
->>>>>>> parent of 391f19b... komicik
-=======
->>>>>>> parent of 391f19b... komicik
+
     }
 }
